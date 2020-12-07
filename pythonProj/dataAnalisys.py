f1cc5ab75235481c5616609125f2629b725806b1
BenjamimOliveira <bmoliveira03@gmail.com>
2020-12-07 16:57:09 +0000

    Inicio da exploração de dados - histograma relativo a capital gain

--- pythonProj/dataAnalisys.py
+++ pythonProj/dataAnalisys.py
@@ -9,10 +9,16 @@ import seaborn as sns
 # -- var do tipo pandas.core.frame.DataFrame
 training = pd.read_csv('training.csv', sep=';', engine='python')
 
+# -- alguma informação sobre o dataset
 #print(training.shape)
-print(training.info())
-print(training.describe())
+#print(training.info())
+#print(training.describe())
 
-# -- Pairplot - pesado
+# -- Pairplot - pesado - evitar
 #sns.pairplot(training)
+#plt.show()
+
+# -- Histograma relativo a capital-gain
+# -- Capital Gain - diferença entre o valor de revenda de um bem e o seu valor de compra
+#training[' capital-gain'].plot(kind='hist')
 #plt.show()
