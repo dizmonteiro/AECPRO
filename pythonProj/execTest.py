8f3474cea9c8a02b609979ee6dfa1ddf5f0c0416
BenjamimOliveira <bmoliveira03@gmail.com>
2021-01-06 22:05:31 +0000

    Added jupyter notebooks for simplicity
    Now we have 1 notebook per process

--- pythonProj/execTest.py
+++ pythonProj/execTest.py
@@ -2,7 +2,7 @@ from algorithms.SupportVectorMachines import SupportVectorMachines
 from algorithms.ensembleLearner import EnsembleLearner
 from algorithms.neuralNetwork import NeuralNetwork
 
-lr = NeuralNetwork('data/processedDataTraining.csv', 'data/processedDataTest.csv', '')
-lr.gridSearch()
-#lr.fit()
-#lr.predict()
+lr = EnsembleLearner('data/processedDataTraining.csv', 'data/processedDataTest.csv')
+#lr.gridSearch()
+lr.fit()
+lr.predict()
